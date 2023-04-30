c550ca8f3508f99d6fc7443ee18cbc237b48a196
= <--unset>
2023-04-30 01:29:22 +0000

    Remove timestamps from permissions table

--- database/migrations/2023_04_26_022621_create_permissions_table.php
+++ database/migrations/2023_04_26_022621_create_permissions_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
-            $table->string('code');
-            $table->timestamps();
+            $table->string('code')->unique();
         });
     }
 
