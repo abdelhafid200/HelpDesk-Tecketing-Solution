52f0b1f54880428e68681279da9c947f39a26bb5
= <--unset>
2023-04-30 03:55:07 +0000

    Change the order of calendar migrations

---
+++ database/migrations/2023_04_24_031436_create_days_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('days', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('day');
+            $table->time('start');
+            $table->time('end');
+            $table->foreignId('calendar_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('days');
+    }
+};
